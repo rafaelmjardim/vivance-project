338d9392142cd575cbe514bad3b7ef5654d5ec7b
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-23 22:11:47 -0300

    feat: adicionado conteudo da pagina hero

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Header } from "./ui/header";
 import heroBackground from "@/public/home-backgorund.jpg";
+import { Button } from "./ui/button";
 
 export function Hero() {
   return (
@@ -18,10 +19,20 @@ export function Hero() {
         <div className="absolute inset-0 bg-linear-to-r from-black/80 via-black/60 to-black/40"></div>
       </div>
 
-      <article className="block z-10">
-        <h1 className="text-text-inverse text-6xl max-w-150">
+      <article className="grid gap-6 z-10 max-w-150">
+        <h1 className="text-text-inverse text-6xl">
           Construção Moderna em Steel Frame
         </h1>
+
+        <p className="text-text-inverse-secundary text-sm leading-6">
+          Rapidez, sustentabilidade e qualidade superior. Transforme seu projeto
+          em realidade com a tecnologia mais avançada em construção civil.
+        </p>
+
+        <div className="flex items-center gap-4">
+          <Button>Solicitar orçamento</Button>
+          <Button isTransparent={true}>Ver catálogo</Button>
+        </div>
       </article>
     </section>
   );

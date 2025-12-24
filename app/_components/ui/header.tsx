28bb9230a9eb6841e4e92f5b4b355277c0b8f228
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-23 23:14:36 -0300

    feat: adiciona responsivo com icon de menu mobile

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "@/public/logo.png";
+import { HiMiniBars3 } from "react-icons/hi2";
 
 type Menu = {
   txt: string;
@@ -16,18 +17,15 @@ export function Header() {
     { txt: "Outros serviços", href: "outros-servicos" },
   ];
   return (
-    <header
-      className="py-6 px-4 container sm:px-24 mx-auto 
-      absolute z-10 top-0 left-0 right-0 flex item-center justify-between w-full"
-    >
+    <header className="py-6 px-4 container sm:px-24 mx-auto absolute z-10 top-0 left-0 right-0 flex items-center justify-between w-full">
       <div className="flex items-center gap-4">
         <Image src={logo} alt="Logo do Vivance Incorporadora" height={40} />
-        <span className="text-2xl text-text-inverse">
+        <span className="hidden xl:inline-flex text-2xl text-text-inverse">
           Vivance Incorporadora
         </span>
       </div>
 
-      <ul className="text-text-inverse flex items-center gap-8">
+      <ul className="hidden lg:inline-flex tems-center text-text-inverse gap-8">
         {menuArray.map((menu, index) => (
           <li
             key={index}
@@ -37,6 +35,12 @@ export function Header() {
           </li>
         ))}
       </ul>
+
+      <HiMiniBars3
+        className="lg:hidden"
+        color="var(--color-icon-inverse)"
+        size={24}
+      />
     </header>
   );
 }

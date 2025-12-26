072d5f4f70cf2c4578988b9a8a9e82459727bea0
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-26 10:29:49 -0300

    feat: adiciona responsivo dos buttons

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -7,7 +7,7 @@ import { MouseAnimate } from "./ui/mouseAnimate";
 
 export function Hero() {
   return (
-    <section className="relative flex items-center h-screen bg-gray-950 px-48">
+    <section className="relative flex items-center h-screen bg-gray-950">
       <Header />
 
       <div className="absolute inset-0">
@@ -21,22 +21,25 @@ export function Hero() {
         <div className="absolute inset-0 bg-linear-to-r from-black/80 via-black/60 to-black/40"></div>
       </div>
 
-      <article className="grid gap-6 z-10 max-w-150">
-        <h1 className="text-text-inverse text-6xl leading-16">
-          Construção Moderna em Steel Frame
-        </h1>
+      <article className="container mx-auto px-4 sm:px-24 z-10">
+        <div className="grid gap-4 max-w-150">
+          <h1 className="text-text-inverse text-3xl sm:text-4xl lg:text-6xl ">
+            Construção Moderna em Steel Frame
+          </h1>
 
-        <p className="text-text-inverse-secundary text-sm leading-6">
-          Rapidez, sustentabilidade e qualidade superior. Transforme seu projeto
-          em realidade com a tecnologia mais avançada em construção civil.
-        </p>
+          <p className="text-text-inverse-secundary text-base sn:text-lg md:text-xl">
+            Rapidez, sustentabilidade e qualidade superior. Transforme seu
+            projeto em realidade com a tecnologia mais avançada em construção
+            civil.
+          </p>
 
-        <div className="flex items-center gap-4">
-          <Button>
-            Solicitar orçamento
-            <HiArrowSmallRight size={20} />
-          </Button>
-          <Button isTransparent={true}>Ver catálogo</Button>
+          <div className="flex items-center gap-4 flex-col sm:flex-row">
+            <Button>
+              Solicitar orçamento
+              <HiArrowSmallRight size={20} />
+            </Button>
+            <Button isTransparent={true}>Ver catálogo</Button>
+          </div>
         </div>
       </article>
 
--- app/_components/ui/button.tsx
+++ app/_components/ui/button.tsx
@@ -6,8 +6,8 @@ export function Button({ children, isTransparent }: ButtonProps) {
   return (
     <button
       data-transparent={isTransparent}
-      className="text-text-inverse text-sm w-fit py-4 px-8 rounded-[.625rem] bg-surface-primary hover:bg-surface-hover-primary
-      transition-all cursor-pointer flex items-center gap-2 outline-none
+      className="text-text-inverse text-sm py-4 px-8 rounded-[.625rem] bg-surface-primary hover:bg-surface-hover-primary
+      transition-all cursor-pointer w-full sm:w-fit flex items-center justify-center sm:justify-start gap-2 outline-none
       data-[transparent=true]:bg-white/10 data-[transparent=true]:backdrop-blur-sm data-[transparent=true]:hover:bg-white/20"
     >
       {children}

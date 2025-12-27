fb5342a1098b3843a230bab9e5039d2db317cb23
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-26 22:04:57 -0300

    refactor: implementa slider no lugar de imagem fixa

--- app/_components/aboutSteelFrame.tsx
+++ app/_components/aboutSteelFrame.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import steelImage from "@/public/steel.png";
+import SliderImage from "./sliderImage";
 
 export default function AboutStellFrame() {
   return (
@@ -37,15 +36,10 @@ export default function AboutStellFrame() {
         </div>
       </article>
 
-      <div className="relative max-w-87 sm:max-w-120 lg:max-w-134 shadow-lg rounded-2xl">
-        <Image
-          src={steelImage}
-          alt="Imagem casa feita em steel frame"
-          width={640}
-          className="rounded-2xl"
-        />
+      <div className="relative max-w-87 sm:max-w-120 lg:max-w-134 rounded-2xl">
+        <SliderImage />
 
-        <div className="absolute -bottom-6 -left-6 w-fit bg-surface-primary shadow-lg p-8 rounded-[.875rem]">
+        <div className="absolute -bottom-6 -left-6 w-fit bg-surface-primary shadow-lg p-6 rounded-[.875rem]">
           <h2 className="text-md sm:text-base text-text-inverse max-w-52.75">
             Tecnologia de ponta para construções residenciais e comerciais
           </h2>

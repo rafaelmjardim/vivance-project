d19bd2d978a7d6b6c3608cc552884077c7c76ae8
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-28 20:02:26 -0300

    fix: ajustado ordem do menu

--- app/constants/menu.ts
+++ app/constants/menu.ts
@@ -6,8 +6,8 @@ type Menu = {
 export const menuArray: Menu[] = [
   { txt: "Steel Frame", sectionId: "aboutSteelFrame" },
   { txt: "Benefícios", sectionId: "benefitsSteelFrame" },
-  { txt: "Contato", sectionId: "contact" },
   { txt: "Catálogo de projetos", sectionId: "projectCatalog" },
   { txt: "Processos", sectionId: "process" },
+  { txt: "Contato", sectionId: "contact" },
   { txt: "Outros serviços" },
 ];

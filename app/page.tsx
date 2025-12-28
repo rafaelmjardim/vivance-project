29059a7052e2cfab4348b353326dae7656c8e92e
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-28 14:53:01 -0300

    feat: implementa tela de processo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { AboutStellFrame } from "./_components/aboutSteelFrame";
 import { BenefitsSteelFrame } from "./_components/benefitsSteelFrame";
 import { Footer } from "./_components/footer";
 import { Hero } from "./_components/hero";
+import { Process } from "./_components/process";
 import { ProjectCatalog } from "./_components/projectCatalog";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <AboutStellFrame />
       <BenefitsSteelFrame />
       <ProjectCatalog />
+      <Process />
       <Footer />
     </main>
   );

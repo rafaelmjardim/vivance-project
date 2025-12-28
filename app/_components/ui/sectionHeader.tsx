80430f57c2a5c39fcf4dc4b22c6ba8b72500872b
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-28 14:46:41 -0300

    feat: implementa prop de color inverse

--- app/_components/ui/sectionHeader.tsx
+++ app/_components/ui/sectionHeader.tsx
@@ -1,13 +1,28 @@
 type SectionHeaderProps = {
   title: string;
   description: string;
+  isColorInverse?: boolean;
 };
 
-export function SectionHeader({ title, description }: SectionHeaderProps) {
+export function SectionHeader({
+  title,
+  description,
+  isColorInverse,
+}: SectionHeaderProps) {
   return (
     <div className="text-center flex flex-col items-center">
-      <h2 className="text-3xl lg:text-4xl mb-6">{title}</h2>
-      <p className="text-text-secundary text-md sm:text-base max-w-140">
+      <h2
+        className={`text-3xl lg:text-4xl mb-6 ${
+          isColorInverse ? "text-text-inverse" : "text-text-default"
+        }`}
+      >
+        {title}
+      </h2>
+      <p
+        className={`text-md sm:text-base max-w-140 ${
+          isColorInverse ? "text-text-grey-light" : "text-text-secundary"
+        }`}
+      >
         {description}
       </p>
     </div>
